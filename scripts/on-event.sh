5ff7aa5b8e52e10d10e45ea8e2f7cbebc86758bf
Guy Moses <guy.moses@dash0.com>
2026-06-08 10:49:41 +0300

    release: v0.1.9

--- scripts/on-event.sh
+++ scripts/on-event.sh
@@ -43,7 +43,7 @@ load_settings "$PROJECT_SETTINGS" || load_settings "$GLOBAL_SETTINGS" || true
 PLUGIN_DATA="${CLAUDE_PLUGIN_DATA:?CLAUDE_PLUGIN_DATA not set}"
 BIN_DIR="$PLUGIN_DATA/bin"
 REPO="dash0hq/dash0-agent-plugin"
-VERSION="0.1.8"
+VERSION="0.1.9"
 
 # Detect OS and architecture.
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
